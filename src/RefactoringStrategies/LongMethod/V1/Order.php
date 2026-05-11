794d6a807b5559256ee76833568fad4c041a0a36
devengineer <wixphp@gmail.com>
2026-05-11 14:12:35 +0300

    add class Order

---
+++ src/RefactoringStrategies/LongMethod/V1/Order.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\RefactoringStrategies\LongMethod\V1;
+
+class Order
+{
+    private int $discount = 0;
+    private array $items = [];
+    private Customer $customer;
+
+    public function __construct(Customer $customer)
+    {
+        $this->customer = $customer;
+    }
+
+    public function getCustomer(): Customer
+    {
+        return $this->customer;
+    }
+
+    public function addItems(array $items): void
+    {
+        $this->items = array_merge($this->items, $items);
+    }
+
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    public function getDiscount(): int
+    {
+        return $this->discount;
+    }
+
+    public function setDiscount(int $discount): void
+    {
+        $this->discount = $discount;
+    }
+
+    public function hasDiscount(): bool
+    {
+        return !empty($this->getDiscount());
+    }
+
+    public function calculate(): float
+    {
+        $total = 0;
+        $items = $this->getItems();
+
+        foreach ($items as $item) {
+            if ($item->hasVat()) {
+                $vat = $item->getVat();
+            } elseif ($this->getCustomer()->hasVat()) {
+                $vat = $this->getCustomer()->getVat();
+            } else {
+                $vat = 0;
+            }
+
+            $price = $item->getPrice() * $item->getQuantity();
+            $total += $price + ($price / 100 * $vat);
+        }
+
+        if ($this->hasDiscount()) {
+            $total = $total - ($total / 100 * $this->getDiscount());
+        } elseif ($this->getCustomer()->hasDiscountForMaxAmount() && $total >= $this->getCustomer()->getMaxAmountForDiscount()) {
+            $total = $total - ($total / 100 * $this->getCustomer()->getDiscountForMaxAmount());
+        }
+
+        return round($total, 2);
+    }
+
+}
